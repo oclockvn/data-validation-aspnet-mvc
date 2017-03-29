0e19bb69f6e10732e9c0f78faede84ef072b03cc
oclockvn <oclockvn@gmail.com>
2017-03-29 18:48:00 +0700

    init

---
+++ WebApplication1/Models/Product.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace WebApplication1.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Product name")]
+        [Required(ErrorMessage = "{0} is required")]
+        [StringLength(250, MinimumLength = 2, ErrorMessage = "{0} must be from {2} to {1} characters")]
+        public string Name { get; set; }
+
+        [Display(Name = "Poduct price"), Required(ErrorMessage = "{0} is required")]
+        public double Price { get; set; }
+
+        [Display(Name = "Price description")]
+        public string Description { get; set; }
+
+        [Display(Name = "Product snippet")]
+        [StringLength(250, ErrorMessage = "{0} is too long")]
+        public string Snippet { get; set; }
+    }
+}
---
+++ WebApplication1/Modules/LogModule.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Modules
+{
+    public class LogModule : IHttpModule
+    {
+        public void Dispose()
+        {
+            
+        }
+
+        public void Init(HttpApplication context)
+        {
+            context.BeginRequest += (src, args) =>
+            {
+                Debug.WriteLine("log module begin request");
+            };
+        }
+    }
+}
