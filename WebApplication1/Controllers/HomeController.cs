c872620f970037de0a4161778498e088920ce3cf
oclockvn <oclockvn@gmail.com>
2017-03-30 13:09:30 +0700

    add validate helper

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
 {
@@ -11,6 +12,17 @@ namespace WebApplication1.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateProduct(Product model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Index", model);
+            }
+
+            return View("Index");
         }
     }
 }
